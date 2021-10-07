99135417f9d820889cd8a5671404d9b8feafdde2
Alejandro <alejandro@Alejandros-iMac.home>
2021-10-06 22:45:43 -0300

    agrego Tests en Zoo/Animales

--- test/zoologico/TestZoo.java
+++ test/zoologico/TestZoo.java
@@ -6,8 +6,23 @@ import org.junit.Test;
 public class TestZoo {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queSePuedaAgregarAnimalesAlZoo(){
+	}
+	
+	@Test
+	public void queSePuedaEmitirEntrada(){
+	}
+	
+	@Test
+	public void queSePuedaCalcularAlimentoPorArea(){
+	}
+	
+	@Test
+	public void queSePuedaObtenerCantidadTotalAnimalesEnElZoo(){
 	}
 
 }
+
+
+
+
