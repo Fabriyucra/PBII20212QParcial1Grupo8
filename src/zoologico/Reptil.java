e18f5750963bd6f755c9486b493f19c354d8a6a2
Ivancitoo <ivamaldonado@alumno.unlam.edu.ar>
2021-10-07 02:36:06 -0300

    agrego metodos animales

--- src/zoologico/Reptil.java
+++ src/zoologico/Reptil.java
@@ -4,8 +4,8 @@ public class Reptil extends Animal {
 
 	private String tipoEscamas;
 	
-	public Reptil(String nombre, String reproduccion, Double peso, Double alimento, String tipoEscamas) {
-		super(nombre, reproduccion, peso, alimento);
+	public Reptil(String nombre, String sexo, String reproduccion, Double peso, Double alimento, String tipoEscamas) {
+		super(nombre, sexo, reproduccion, peso, alimento);
 		this.tipoEscamas = tipoEscamas;
 		setArea(TipoArea.REPTILES);
 		setTipoSangre(TipoSangre.ECTOTERMO);
